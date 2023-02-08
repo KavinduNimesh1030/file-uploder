a62845c26a5c93c52058bb29fa45c595bfb494d5
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-08 22:39:31 +0530

    controller class created

--- src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.servlet.annotation.MultipartConfig;
+
 @Configuration
 @ComponentScan(basePackageClasses = {FileUploaderController.class})
 @EnableWebMvc
+@MultipartConfig
 public class WebAppConfig {
     public WebAppConfig() {
         System.out.println("WebAppConfig");
--- src/main/java/lk/ijse/spring/controller/FileUploaderController.java
+++ src/main/java/lk/ijse/spring/controller/FileUploaderController.java
@@ -1,11 +1,36 @@
 package lk.ijse.spring.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
-@EnableWebMvc
+import javax.servlet.annotation.MultipartConfig;
+import java.io.File;
 
+@RestController
+@RequestMapping("/upload")
+@CrossOrigin
+@MultipartConfig
 public class FileUploaderController {
+    @GetMapping()
+    public String hello() {
+        return "uploader";
+    }
 
+    @PostMapping()
+    public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file ) {
+
+        String fileName = file.getOriginalFilename();
+        System.out.println(fileName);
+        try {
+            file.transferTo( new File("C:\\upload\\" + fileName));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.ok("File uploaded successfully.");
+    }
 }
